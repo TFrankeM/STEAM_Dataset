821d4f74668e30b3626876b10cd3395a5d15db8a
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-11-03 12:20:55 -0300

    Update deletar_tabelas.sql

--- deletar_tabelas.sql
+++ deletar_tabelas.sql
@@ -1,18 +1,26 @@
-DROP TABLE Lê
-DROP TABLE Suporte;
+/*Excluir tabelas do nanco de dados da steam*/
+
+DROP TABLE Joga;
+DROP TABLE ClassificadoComo;
+DROP TABLE Inclui;
+DROP TABLE Desenvolve;
+DROP TABLE InformeSobre;
+DROP TABLE Lê;
 DROP TABLE InterageEm;
+DROP TABLE Curte;
+DROP TABLE Desenvolvedora;
+DROP TABLE Suporte;
 DROP TABLE Comentário;
 DROP TABLE Fórum;
 DROP TABLE Gênero;
 DROP TABLE Aquisição;
-/*DROP TABLE Avaliação;*/
+DROP TABLE Avaliação;
 DROP TABLE Notícia;
 DROP TABLE ItemJogo;
 DROP TABLE Empresa;
 DROP TABLE ListaDeDesejos;
 DROP TABLE BibliotecaDeJogos;
 DROP TABLE Jogo;
-DROP TABLE Curte;
 DROP TABLE Conteúdo;
 DROP TABLE Categoria;
 DROP TABLE Usuário;
