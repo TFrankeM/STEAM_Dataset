c20ac96c99cbcec47ee461db1f5fa31a44d42373
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-11-16 16:30:53 -0300

    Update indice_boladao.sql

--- indice_boladao.sql
+++ indice_boladao.sql
@@ -1,3 +1,8 @@
 /* Código que cria um nonclustered index para a tabela Aquisição */
 
 create nonclustered index ID_boladão on Aquisição (DataAquisição);
+
+select count(*) as transações, dataaquisição from aquisição
+where dataaquisição between '2018-01-01' and '2021-12-31'
+group by dataaquisição
+order by dataaquisição desc;
